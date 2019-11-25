339f7c59d3d02f869f719162353583e4ee206b3e
i.krupnov <i.krupnov@redmadrobot.com>
2019-11-25 20:31:52 +0300

    Init Continue

--- code.ts
+++ code.ts
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './uikit/normalize.sass';
-// import {HotApp, App} from './App';
-//
-// const root = document.getElementById('root');
-//
-// // @ts-ignore
-// if (module.hot) {
-//   ReactDOM.render(<HotApp />, root);
-// } else {
-//   ReactDOM.render(<App />, root);
-// }
-
 // This plugin will open a modal to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
